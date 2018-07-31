a603a1692403dc2336160644495bd4dc072a47be
Lawrence <lawrence_gandhar@yahoo.com>
2018-08-01 03:21:31 +0530

    uploaded

---
+++ project/app/models.py
@@ -0,0 +1,85 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from datetime import datetime
+
+
+# Create your models here.
+
+#
+# ROLES MODEL
+#
+class Roles(models.Model):
+    
+    name = models.CharField(max_length = 20, db_index = True, unique = True,)
+    is_active = models.BooleanField(default = True, db_index = True,)
+
+    def __str__(self):
+        return self.name.title()
+
+    class Meta:
+        verbose_name_plural = "roles_table"
+        ordering = ['pk']
+
+
+#
+# Custom User Model. Extended from the default user model
+#
+class CustomUser(AbstractUser):
+
+    #
+    # Title Choices
+    #
+    title_choices = (('Mr','Mr'),('Mrs','Mrs'),('Miss','Miss'),('Ms','Ms'),('Sir','Sir'))
+
+    #
+    # Gender Choices
+    #
+    MALE = 'M'
+    FEMALE = 'F'
+    OTHERS = 'O'
+    gender_choices = ((MALE,'Male'),(FEMALE, 'Female'), (OTHERS, 'Others')) 
+
+    #
+    # Marital Status Choices
+    #
+    marital_status_choices = (("Married", "Married"),
+                                ("Divorced", "Divorced"),
+                                ("Separated", "Separated"),
+                                ("Living With Partners", "Living With Partners"),
+                                ("Single", "Single"),
+                                ("Widowed", "Widowed"))
+                                
+    #
+    # Model Structure
+    #
+    role = models.ForeignKey('Roles', db_index = True, null = True, blank = True, on_delete = models.SET_NULL,)
+    dob = models.DateField(null = True, blank = True, db_index = True,)
+    phone = models.CharField(max_length = 100, blank = True, null = True,)
+    title = models.CharField(max_length = 4, db_index = True, blank = True, null = True, choices = title_choices,)
+    gender = models.CharField(max_length = 1, db_index = True, default = MALE, choices = gender_choices, blank = True, null = True,)
+    marital_status = models.CharField(max_length = 40, db_index = True, default = 'Single', blank = True, null = True, choices = marital_status_choices,)
+    email = models.EmailField(blank = True, null = True, db_index = True,)
+    postcode = models.CharField(max_length = 10, null = True, blank = True, db_index = True,)
+    city = models.CharField(max_length = 30, null = True, blank = True, db_index = True,)
+    state = models.CharField(max_length = 30, null = True, blank = True, db_index = True,)
+    address_line_1 = models.CharField(max_length = 100, null = True, blank = True, db_index = True,)
+    address_line_2 = models.CharField(max_length = 100, null = True, blank = True,)
+    country = models.CharField(max_length = 30, null = True, blank = True, db_index = True,)
+    acknowledge = models.BooleanField(default = True, db_index = True)
+    sponsor = models.BooleanField(default = True, db_index = True)
+    income = models.FloatField(default = 0, db_index = True,)
+    home_owner = models.BooleanField(default = False, db_index = True,)
+    browser = models.CharField(max_length = 250, null = True, blank = True, db_index = True,)
+    bversion = models.CharField(max_length = 250, null = True, blank = True, db_index = True,)
+    platform = models.TextField(null = True, blank = True,)
+    ip_address = models.CharField(max_length = 20, null = True, blank = True,)
+    extra_fields = models.TextField(null = True, blank = True,)
+    inserted_date = models.DateTimeField(auto_now = False, auto_now_add = True, db_index = True,)
+    modified_date = models.DateTimeField(auto_now = True, null = True, blank = True, db_index = True,)
+
+
+
+
+
+    
---
+++ project/app/static/admin_js/usermodel.js
@@ -0,0 +1,192 @@
+(function ($) { 
+    'use strict';
+    
+    $(document).ready(function() {
+
+        /* Edit Form Details Start */ 
+        var pathname = window.location.pathname;
+		var path_id = pathname.split("/");
+        
+        var user_details = "";
+
+		if(path_id[path_id.length - 2] == "change"){
+			
+			var user_id = parseInt(path_id[path_id.length - 3]);
+			
+			$.post("/admin-userform-details/",{'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val(),"id":user_id},function(data){
+                user_details = $.parseJSON(data);
+                location_change($("select#id_location option:selected").val(), user_details);
+            });
+        }
+        /* Edit Form Details End */ 
+
+
+
+       /*
+       # Setting field order in the admin user form. 
+       */     
+
+        var field_username = '<div class="form-row field-username">'+$(".field-username").html()+"</div>";
+        var field_last_login = '<div class="form-row field-last_login">'+$(".field-last_login").html()+"</div>";
+        var field_user_permissions = '<div class="form-row field-user_permissions">'+$(".field-user_permissions").html()+"</div>";
+        var field_groups = '<div class="form-row field-groups">'+$(".field-groups").html()+'</div>';
+        var field_date_joined = '<div class="form-row field-date_joined">'+$(".field-date_joined").html()+'</div>';
+        var field_is_superuser = '<div class="form-row field-is_superuser">'+$(".field-is_superuser").html()+'</div>';
+
+        $(".field-username").remove();
+        $(".field-last_login").remove();
+        $(".field-user_permissions").remove();
+        $(".field-groups").remove();
+        $(".field-date_joined").remove();
+        $(".field-is_superuser").remove();
+
+        $(".field-password").before(field_username);
+        $(".field-is_staff").after(field_user_permissions).after(field_groups);
+        $(".field-assigned_to").after(field_date_joined);
+        $(".field-date_joined").after(field_last_login);
+        $(".field-email").after(field_is_superuser);
+
+        $(".field-user_permissions").hide();
+        $(".field-groups").hide();
+
+        
+        /*
+        # Since super user designates that this user has all permissions without explicitly assigning them. 
+        # if is_admin is selected, will hide permissions and group dowdowns.
+        # Else if_staff, show permissions and group dowdowns.
+        */
+
+        $('[name="is_staff"]').change(function()
+        {
+            if ($(this).is(':checked')) {
+                $(".field-user_permissions").show();
+                $(".field-groups").show();
+            }else{
+                $(".field-user_permissions").hide();
+                $(".field-groups").hide();
+            }
+        });
+
+        $('[name="is_superuser"]').change(function()
+        {
+            $('#id_is_staff').prop("checked",false);
+            $(".field-user_permissions").hide();
+            $(".field-groups").hide();
+        });
+
+        /* 
+        Default values set to blank in select fields
+        */
+
+        $("select#id_department").empty();
+        $("select#id_designation").empty();
+
+        /* 
+        Add a parent select for Department
+        */
+       $("select#id_department").before('<select style="margin-right:10px;" name="department_parents" id="id_department_parents"></select>')
+
+
+        /*
+        Populate department field on location change. 
+        */
+
+        $("select#id_location").change(function(){
+            $("select#id_department_parents, select#id_department, select#id_designation,select#id_assigned_to").empty();
+            location_change($("select#id_location").val(), user_details)
+        });  
+
+        /*
+        Populate child department field on parent department change. 
+        */
+
+        $("select#id_department_parents").change(function(){
+            //console.log(user_details);
+            $("select#id_department, select#id_designation,select#id_assigned_to").empty();
+            department_change($("select#id_department_parents").val(), user_details)
+        });
+
+        /*
+        Populate child department field on parent department change. 
+        */
+
+        $("select#id_department").change(function(){
+            $("select#id_designation,select#id_assigned_to").empty();
+            designation_change($("select#id_department").val(), user_details)
+        });
+        
+        /* 
+        Dropdown values for already selected fields
+        */   
+       
+    });
+
+    function location_change(id, user_details){
+        $.post("/location-select/",{'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val(),"id":id},function(data){
+           
+            var fetched_data = '<option value="">---------</option>';
+
+            $.each($.parseJSON(data),function(i,v){
+                if(user_details.parent_department == v.id)
+                    fetched_data += '<option value="'+v.id+'" selected>'+v.name+' ( '+v.abbr+' )</option>';
+                else
+                    fetched_data += '<option value="'+v.id+'">'+v.name+' ( '+v.abbr+' )</option>';
+            });
+
+            $("select#id_department_parents").empty().html(fetched_data);
+            department_change($("select#id_department_parents option:selected").val(), user_details);
+        });
+    }
+
+    function department_change(id, user_details){
+        $.post("/department-select/",{'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val(),"id":id},function(data){
+            
+            var fetched_data = '<option value="">---------</option>';
+
+            $.each($.parseJSON(data),function(i,v){
+                if(user_details.department_id == v.id)
+                    fetched_data += '<option value="'+v.id+'" selected>'+v.name+' ( '+v.abbr+' )</option>';
+                else
+                    fetched_data += '<option value="'+v.id+'">'+v.name+' ( '+v.abbr+' )</option>';
+            });
+
+            $("select#id_department").empty().html(fetched_data);
+            designation_change($("select#id_department option:selected").val(), user_details);
+           
+        });
+    }
+
+    function designation_change(id, user_details){
+        $.post("/designation-select/",{'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val(),"id":id},function(data){
+            
+            var fetched_data = '<option value="">---------</option>';
+
+            $.each($.parseJSON(data),function(i,v){
+                if(user_details.designation_id == v.id)
+                    fetched_data += '<option value="'+v.id+'" selected>'+v.name+' ( '+v.abbr+' )</option>';
+                else
+                    fetched_data += '<option value="'+v.id+'">'+v.name+' ( '+v.abbr+' )</option>';
+            });
+
+            $("select#id_designation").empty().html(fetched_data);
+            user_list(id,user_details);
+        });
+    }
+
+
+    function user_list(id, user_details){
+        $.post("/admin-userform-list/",{'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val(),"id":id, "usertype_id":$("select#id_usertype option:selected").val()},function(data){
+            var fetched_data = '<option value="">---------</option>';
+
+            $.each($.parseJSON(data),function(i,v){
+                if(user_details.assigned_to_id == v.id)
+                    fetched_data += '<option value="'+v.id+'" selected>'+v.name+'</option>';
+                else
+                    fetched_data += '<option value="'+v.id+'">'+v.name+'</option>';
+            });
+
+            $("select#id_assigned_to").empty().html(fetched_data);
+        });
+    }
+
+})(django.jQuery);
